7c39a8ca468da9e1b6a2b98aca6f5eeb10654304
Uxdesigns <uxdesigns289@gmail.com>
2023-09-03 19:43:29 +0530

    changed thank-you & added email validation

--- src/views/Newsletter.vue
+++ src/views/Newsletter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="main-container">
+    <div class="main-container" v-if="active">
         <div class="container-one">
             <h1>Stay updated!</h1>
             <p>Join 60,000+ product managers recieving monthly <br> updates on:</p>
@@ -24,6 +24,7 @@
                 <button 
                     class="sub-btn"
                     @click="submit()"
+                    @mouseover="hoverEffect()"
                     ref="btn"
                     title="Click for the error"
                 > Subscribe to monthly newsletter</button>
@@ -33,21 +34,61 @@
         <div class="side-image"></div>
     </div>
 
+
+    <div class="main-container thank-container" v-else>
+        <div class="container-one">
+            <img src="/icon-success.svg" style="margin-bottom:30px;">
+            
+            <h1 style="line-height:55px;margin-bottom: 20px;">Thanks for <br> subscribing!</h1>
+            
+            <p style="margin-bottom:20px;">A confirmation email has been sent to <br> <b>as@gmail.com</b> Please open it and click <br> the button inside the to confirm your subscription</p>
+
+            <div>
+                <button 
+                    class="sub-btn" 
+                    ref="btn" 
+                    @click="dismissMsg()"
+                    @mouseover="hoverEffect()">Dismiss message</button>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
     export default {
+
+        data() {
+            return {
+                active: true
+          }
+        },
+
         methods: {
             submit() {
-                if (! this.$refs['email'].value) {
+                var email_pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+                const validEmail = email_pattern.test(this.$refs['email'].value);
+
+                if (! this.$refs['email'].value || ! validEmail) {
                     this.$refs['error'].style.color = "red";
                     this.$refs['error'].style.display = "block";
 
                     this.$refs['email'].style.border = "1px solid red";
-                    
-                    this.$refs['btn'].style.backgroundImage = "inherit";
-                    this.$refs['btn'].style.backgroundColor = "hsl(234, 29%, 20%)";
+                } else {
+                    this.active = false;
                 }
+            },
+            
+            hoverEffect() {
+                this.$refs['btn'].style.backgroundImage = "inherit";
+                this.$refs['btn'].style.backgroundColor = "hsl(234, 29%, 20%)";
+            },
+
+            dismissMsg() {
+                this.active = true;
+                this.$refs['btn'].style.backgroundImage = "inherit";
+                this.$refs['btn'].style.backgroundColor = "hsl(234, 29%, 20%)";
             }
         }
     };
